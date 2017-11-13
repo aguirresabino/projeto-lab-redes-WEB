d8958515ce975563aa6287d066232aadc58407bc
Aguirre Sabino <aguirresabino187@gmail.com>
2017-11-12 22:16:56 -0300

    Implementando login

--- src/app/controllers/login.controller.js
+++ src/app/controllers/login.controller.js
@@ -1,10 +1,24 @@
 (function(){
 	'use strict';
 
-	angular.module('LoginController', [])
-		.controller('LoginController', ['$location', '$rootScope', LoginController]);
+	angular.module('LoginController', ['UserService'])
+		.controller('LoginController', ['UserService', '$location', LoginController]);
 
-	function LoginController($location, $rootScope){
+	function LoginController(UserService, $location){
 		var vm = this;
+
+		vm.login = function(login){
+			console.log(login)
+			UserService.login(login)
+				.then(function(success){
+					var token = success.data.password;
+					localStorage.setItem('token', token);
+					alert('nome:', success.data.nome);
+				})
+				.catch(function(error){
+					alert("Dados informados estão incorretos, verifique as informações e tente novamente!");
+					console.log("Erro login aluno:", error);
+				})
+		}
 	}
 })();
