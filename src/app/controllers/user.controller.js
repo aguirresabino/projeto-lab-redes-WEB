220bdeee3541f0aa1c4f9ca7c9a57521ba19c3c8
Aguirre Sabino <aguirresabino187@gmail.com>
2017-11-12 22:18:03 -0300

    Corrigindo escrita da palavra success

--- src/app/controllers/user.controller.js
+++ src/app/controllers/user.controller.js
@@ -9,11 +9,34 @@
 
 		vm.cadastrar = function(user){
 			UserService.create(user)
-				.then(function(succes){
+				.then(function(success){
 					alert('Cadastro realizado!');
 				})
 				.catch(function(error){
 					alert('Cadastro não realizado!');
+					console.log('Erro cadastro de usuário', error);
+				})
+		}
+
+		vm.atualizar = function(user){
+			UserService.update(user)
+				.then(function(success){
+					alert('Usuário atualizado!');
+				})
+				.catch(function(error){
+					alert('Usuário não foi atualizado!');
+					console.log('Erro atualização do usuário', error);
+				})
+		}
+
+		vm.remover = function(id){
+			UserService.remove(id)
+				.then(function(success){
+					alert('Conta removida!');
+				})
+				.catch(function(error){
+					alert('A conta não foi removida!');
+					console.log('Erro remover usuário', error);
 				})
 		}
 	}
